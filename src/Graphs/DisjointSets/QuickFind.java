86c88749cd17c89cbcd17299132bc8d5776dc2b2
JuneTheYi <JuneTheYi@gmail.com>
2021-11-04 00:07:05 -0400

    added QuickUnion and UnionByRank for Graph Trees

--- src/Graphs/DisjointSets/QuickFind.java
+++ src/Graphs/DisjointSets/QuickFind.java
@@ -1,45 +1,59 @@
 package Graphs.DisjointSets;
 
 public class QuickFind {
-    // UnionFind.class
-        private int[] root; // root array
 
-        public QuickFind(int size) { // constructor, size = elements in array
-            root = new int[size]; // size = # of vertices,
-            for (int i = 0; i < size; i++) { //initialize root to have the number of size of vertices
-                root[i] = i; //root will initially be filled with zeroes
-            }
+    private int[] root; // root array
+
+    /*
+    constructor, size = elements in array
+    size = # of vertices/nodes
+    initialize root to have the number of size of vertices
+    root will initially be filled with zeroes
+     */
+    public QuickFind(int size) {
+        root = new int[size];
+        for (int i = 0; i < size; i++) {
+            root[i] = i;
         }
-        //find(int x) = time complexity equal O(1)
-        public int find(int x) { //find function, since every element will be pointing
-            // to its root node, we can just return root[x]
-            return root[x];
-        }
+    }
+    /*
+    find(int x) = time complexity equal O(1)
+    find function, since every element will be pointing
+    to its root node, we can just return root[x]
+     */
+    public int find(int x) {
+        return root[x];
+    }
 
-        public void union(int x, int y) {
-            int rootX = find(x);
-            int rootY = find(y);
-            if (rootX != rootY) {             // time complexity = O(N), going through array
-
-                //if the root nodes are not equal, we'll have to go through the entire root array
-                // once we see a value equal to the root ofy
-                for (int i = 0; i < root.length; i++) { //when the corresponding root node is equal to the node we are
-                    // merging (rootY) then we'll have to change the root node to the new rooot. rootX in this case
-                    if (root[i] == rootY) {
-                        root[i] = rootX;
-                    }
+    /*
+    time complexity = O(N), going through array
+    if the root nodes are not equal, we'll have to go through the entire root array
+    once we see a value equal to the root of y
+    when the corresponding root node is equal to the node we are
+    merging (rootY) then we'll have to change the root node to the new rooot. rootX in this case
+     */
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            for (int i = 0; i < root.length; i++) {
+                if (root[i] == rootY) {
+                    root[i] = rootX;
                 }
             }
         }
-        public boolean connected(int x, int y) { //function determines if X and Y are connected
-            return find(x) == find(y);
-        }
+    }
+    //function determines if X and Y are connected
+    public boolean connected(int x, int y) {
+        return find(x) == find(y);
+    }
+
     public static void main(String[] args) throws Exception {
         QuickFind uf = new QuickFind(10);
         // 1-2-5-6-7 3-8-9 4
-        uf.union(1, 2);
-        uf.union(2, 5);
-        uf.union(5, 6);
+        uf.union(1, 2); // root[2] = 1
+        uf.union(2, 5); // since root[2] = 1, rootX will = 1, root[5] = 1
+        uf.union(5, 6); // since root[5] = 1, rootX will = 1, root[6] = 1
         uf.union(6, 7);
         uf.union(3, 8);
         uf.union(8, 9);
@@ -51,10 +65,3 @@ public class QuickFind {
         System.out.println(uf.connected(4, 9)); // true
     }
 }
-
-    // App.java
-    // Test Case
-//    public class App {
-
-
-//}
