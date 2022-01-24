6f6a202decc4782b4fa8b461a72c0b5dd662435c
June <junetheyi@gmail.com>
2022-01-23 21:37:48 -0500

    updated findIfPathExistsInGraph with more notes

--- src/Graphs/DFS/findIfPathExistsInGraph.java
+++ src/Graphs/DFS/findIfPathExistsInGraph.java
@@ -4,17 +4,29 @@ import java.util.*;
 
 /**
  * leetcode 1971
+ * given: n = nodes, edges = each path, start and end node
+ * Input: n = 3, edges = [[0,1],[1,2],[2,0]], source = 0, destination = 2
+ * Output: true
+ * Explanation: There are two paths from vertex 0 to vertex 2:
+ * - 0 → 1 → 2
+ * - 0 → 2
  */
 public class findIfPathExistsInGraph {
     public boolean validPath(int n, int[][] edges, int start, int end) {
-        List<List<Integer>> adjacency_list = new ArrayList<>();
+        List<List<Integer>> adjacency_list = new ArrayList<>(); // we set up a list of lists
         for (int i = 0; i < n; i++) {
-            adjacency_list.add(new ArrayList<>());
+            adjacency_list.add(new ArrayList<>()); // we add a list for each node that's given (e.g. 3)
         }
 
-        for (int[] edge : edges) {
-            adjacency_list.get(edge[0]).add(edge[1]);
-            adjacency_list.get(edge[1]).add(edge[0]);
+        for (int[] edge : edges) { //for every int array provided in edges
+            adjacency_list.get(edge[0]).add(edge[1]); //for each int array, there's a 0 and 1 index
+            adjacency_list.get(edge[1]).add(edge[0]); // bi-directional?
+            /* best explanation so far: makeSameGroup(e[0] , e[1]); makes sure that e[0] and e[1] belong to the same
+            group and hence have the same parent. Hence makeSameGroup(e[1] , e[0]); becomes a redundant task.
+
+            another similar explaination: The question says that each edge denotes a bidirectional edge.
+            To represent those two edges we are pushing the nodes in the graph.
+             */
         }
 
         Deque<Integer> stack = new ArrayDeque<>();
