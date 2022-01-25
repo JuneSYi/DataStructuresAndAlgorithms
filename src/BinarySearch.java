96c4814bc812c979ac2b1ca6a06bdcce31896317
June <junetheyi@gmail.com>
2022-01-25 13:44:53 -0500

    added non recursion of binary search

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -3,8 +3,10 @@ public class BinarySearch {
         int[] arr = {1, 2, 3, 4, 55, 66, 78};
         int target = 4;
         System.out.println(search(arr, target, 0, arr.length-1));
+        System.out.println(nonRecurs(arr, 4));
     }
 
+    // with recursion
     static int search(int[] array, int target, int start, int end) {
         if (start > end) {
             return -1;
@@ -19,5 +21,23 @@ public class BinarySearch {
             return search(array, target, start, mid-1); // you have to return it
         }
         return search(array, target, mid+1, end);
+    }
+
+    //non recurs
+    static public int nonRecurs(int[] inputOne, int targetNumber) {
+        int start = 0;
+        int end = inputOne.length-1;
+        int mid = (end+start) /2;
+
+        while (inputOne[mid] != targetNumber) {
+            if (targetNumber < inputOne[mid]) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+            mid = (end+start)/2;
+
+        }
+        return mid; //index where targetnumber resides
     }
 }
