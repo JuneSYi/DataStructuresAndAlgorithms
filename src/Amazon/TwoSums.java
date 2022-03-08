6bada146c75e8d3fa919cf57a8dcb4a1e6abadad
June <junetheyi@gmail.com>
2022-03-08 09:16:00 -0500

    started Amazon top questions from Leetcode starting with twosums

---
+++ src/Amazon/TwoSums.java
@@ -0,0 +1,38 @@
+package Amazon;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSums {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) { // i+1 is key so j always stays ahead of i
+                if (nums[i] + nums[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                    return ans;
+                }
+            }
+        }
+        return null;
+    }
+
+    public int[] twoSumHash(int[] nums, int target) {
+        int[] ans = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int j = 0; j < nums.length; j++) {
+            int cur = target - nums[j];
+            if (map.containsKey(cur) && map.get(cur) != j) {
+                ans[0] = map.get(nums[j]);
+                ans[1] = map.get(cur);
+                return ans;
+            }
+            
+        }
+        return null;
+    }
+}
--- src/BinaryTree/Medium/PopulatingNextRightPointersPartTwo.java
+++ src/BinaryTree/Medium/PopulatingNextRightPointersPartTwo.java
@@ -7,22 +7,39 @@ public class PopulatingNextRightPointersPartTwo {
     so you connect the next level and then you just walk through the linked list you've just created and so on.
      */
     public void connect(NodeTree root) {
-        while(root!=null) {
-            NodeTree head=new NodeTree(0),end=head;
-            while(root!=null) {
-                if(root.left!=null) {
-                    end.next=root.left;
-                    end=root.left;
+        while (root != null) {
+            NodeTree head = new NodeTree(0), end = head;
+            while (root != null) {
+                if (root.left != null) {
+                    end.next = root.left;
+                    end = root.left;
                 }
-                if(root.right!=null) {
-                    end.next=root.right;
-                    end=root.right;
+                if (root.right != null) {
+                    end.next = root.right;
+                    end = root.right;
                 }
-                root=root.next;
+                root = root.next;
             }
-            root=head.next;
+            root = head.next;
+        }
     }
 
+    public void connectTwo(NodeTree root) {
+        for (NodeTree head = root; head != null; ) {
+            NodeTree nextHead = new NodeTree(0), nextTail = nextHead;
+            for (NodeTree node = head; node != null; node = node.next) {
+                if (node.left != null) {
+                    nextTail.next = node.left;
+                    nextTail = node.left;
+                }
+                if (node.right != null) {
+                    nextTail.next = node.right;
+                    nextTail = node.right;
+                }
+            }
+            head = nextHead.next;
+        }
+    }
 }
 
 class NodeTree {
@@ -43,4 +60,4 @@ class NodeTree {
         right = _right;
         next = _next;
     }
-};
+}
