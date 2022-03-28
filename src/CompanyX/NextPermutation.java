62f3b7e479e307af59cb7a701ff35d81bd49ec11
June <junetheyi@gmail.com>
2022-03-27 20:54:23 -0400

    small adjustment

--- src/CompanyX/NextPermutation.java
+++ src/CompanyX/NextPermutation.java
@@ -9,7 +9,7 @@ public class NextPermutation {
     /**
      * Good morning! Here's your coding interview problem for today.
      *
-     * This problem was asked by Gotham.
+     * This problem was asked by X.
      *
      * Given a number represented by a list of digits, find the next greater
      * permutation of a number, in terms of lexicographic ordering. If there is not
