71b133d936cd68743ce8449c526e3083161d4961
June Yi <79763915+JuneTheYi@users.noreply.github.com>
2023-05-11 16:56:21 -0700

    Update nonConstructibleChange.py

--- AlgoExperts/Arrays/nonConstructibleChange.py
+++ AlgoExperts/Arrays/nonConstructibleChange.py
@@ -1,3 +1,20 @@
+# O(n) time, O(1) space
+def nonConstructibleChange(coins):
+    # whats the logic here?
+    # sort to start from smallest to biggest, as you go through coins
+    # we add the smalllest numbers if it can make 1, it can't make 2 unless there's another 1 or 2,
+    # it can't make +1
+    # with 1 and a 2 in an array, it can make 1,2,3, it can't make 4 so if next value is > 3 +1, then 4 is highest
+    coins.sort()
+    change = 0
+    for val in coins:
+        if val > change+1:
+            return change+1
+        else:
+            change += val
+    return change+1
+
+
 # failed
 def nonConstructibleChange(coins):
     # whats the pattern? we're trying to count from 1 and +
