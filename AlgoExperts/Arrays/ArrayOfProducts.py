99362b7593ca4e25049fd46f8d237342dfb9e45f
June Yi <79763915+JuneTheYi@users.noreply.github.com>
2023-05-26 15:05:22 -0700

    Update ArrayOfProducts.py

--- AlgoExperts/Arrays/ArrayOfProducts.py
+++ AlgoExperts/Arrays/ArrayOfProducts.py
@@ -1,3 +1,29 @@
+# O(3N) time, O(3N) space, NOT GREAT
+def arrayOfProducts(array):
+    # what if we use 2 pointers, we have every number multiplied up to that point from left to right
+    # another array that shows every number from right to left
+    # left to right 5, 5, 20, 40
+    # right to left 40,8, 8 , 2
+    arrleft = []
+    arright = []
+    start = 1
+    for val in array:
+        start*=val
+        arrleft.append(start)
+    start = 1
+    for val in reversed(array):
+        start*=val
+        arright.insert(0,start)
+    ans = []
+    ans.append(arright[1])
+    for idx,i in enumerate(array[1:],start=1):
+        if idx == len(array)-1:
+            ans.append(arrleft[idx-1])
+            break
+        ans.append(arrleft[idx-1]*arright[idx+1])
+    return ans
+
+
 # bad solution, doesn't solve edge case of 1 zero
 def arrayOfProducts(array):
     # takes in a non-empty array of integers
