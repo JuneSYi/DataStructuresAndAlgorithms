03b7482c0e2b24b800b133a10e8e3497dddfcffe
June Yi <79763915+JuneTheYi@users.noreply.github.com>
2023-05-26 09:02:41 -0700

    Update LongestPeak.py

--- AlgoExperts/Arrays/LongestPeak.py
+++ AlgoExperts/Arrays/LongestPeak.py
@@ -1,3 +1,32 @@
+# spent way too long on this, not sure why it keeps failing
+def longestPeak(array):
+    uptrend = False
+    downtrend = False
+    count = 1
+    ans = 0
+    for idx,val in enumerate(array[1:], start=1):
+        if not uptrend and not downtrend:
+            if array[idx-1] < array[idx]:
+                count = 1
+                uptrend = True
+        if uptrend and array[idx-1]==array[idx]:
+            uptrend = False
+        if uptrend and array[idx-1] > array[idx]:
+            downtrend = True
+            uptrend = False
+        if downtrend: 
+            if array[idx-1] <= array[idx]:
+                if count > ans:
+                    ans = count
+                    count = 1
+                downtrend = False
+        count +=1
+        if idx == len(array)-1 and downtrend:
+            if count > ans:
+                ans = count
+    return ans
+
+
 # fail
 def longestPeak(array):
     # returns length of longest peak
