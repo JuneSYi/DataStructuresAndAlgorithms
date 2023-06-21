25b5b41b46e05265713c11597b85b545c1eb8cc1
June Yi <79763915+JuneSYi@users.noreply.github.com>
2023-06-20 21:39:46 -0700

    Update FindClosestValueInBST.py

--- AlgoExperts/Binary Search Tree/FindClosestValueInBST.py
+++ AlgoExperts/Binary Search Tree/FindClosestValueInBST.py
@@ -1,3 +1,37 @@
+# O(log(n)) time, can be O(n) time, O(n) space ? if recursive, we're calling the recursion fcn multiple times. in this, we add frames on the call stack. hence using extra memory.
+def findClosestValueInBst(tree, target):
+    return recurse(tree,target,tree.value)
+
+def recurse(tree,target,smallest):
+    # check if larger or smaller
+    # if larger then we do .right
+    # if smaller then we do .left
+    # we want to know if the abs(.value-target) of last value is smaller, then current .value
+    # if so then we return the last value, else we recurse with new value
+    if tree is None:
+        return smallest
+    if target == tree.value:
+        return tree.value
+    if abs(target-smallest) > abs(target-tree.value):
+        smallest = tree.value
+    if target > tree.value and tree.right is not None:
+        return recurse(tree.right,target,smallest)
+    elif target > tree.value and tree.right is None:
+        return smallest
+    if target < tree.value and tree.left is not None:
+        return recurse(tree.left,target,smallest)
+    elif target < tree.value and tree.left is None:
+        return smallest
+
+
+# This is the class of the input tree. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
 # fail
 def findClosestValueInBst(tree, target):
     # recursion, base case, if abs(target-curr.value) < abs(target-last.value)
