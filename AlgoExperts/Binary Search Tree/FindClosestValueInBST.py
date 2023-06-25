009a45612a033294d68f84f63e4733a58e70bbec
June Yi <79763915+JuneSYi@users.noreply.github.com>
2023-06-25 16:46:01 -0700

    Update FindClosestValueInBST.py

--- AlgoExperts/Binary Search Tree/FindClosestValueInBST.py
+++ AlgoExperts/Binary Search Tree/FindClosestValueInBST.py
@@ -1,3 +1,23 @@
+# re-visited. solved it faster, still small trouble. O(n) worst case, O(logN) average, O(n) space
+def findClosestValueInBst(tree, target):
+    # bst, greater is right, lower is left
+    # need a last value, if last is closer to target, we retunr that
+    # recurse, base case, if none, return.
+    return recurse(tree,target,tree.value)
+
+def recurse(tree,target,closest):
+    if tree == None:
+        return closest
+    if abs(target-closest) > abs(target-tree.value):
+        closest = tree.value
+    if target > tree.value:
+        return recurse(tree.right,target,closest)
+    elif target < tree.value:
+        return recurse(tree.left,target,closest)
+    else:
+        return closest
+
+
 # O(log(n)) time, can be O(n) time, O(n) space ? if recursive, we're calling the recursion fcn multiple times. in this, we add frames on the call stack. hence using extra memory.
 def findClosestValueInBst(tree, target):
     return recurse(tree,target,tree.value)
