c5f37f003288b7a607a97f44a554adec4624c9bc
June Yi <79763915+JuneSYi@users.noreply.github.com>
2023-07-03 12:45:40 -0700

    Update NodeDepths.py

--- AlgoExperts/BinaryTrees/NodeDepths.py
+++ AlgoExperts/BinaryTrees/NodeDepths.py
@@ -1,3 +1,20 @@
+# O(n) time, O(n) space
+# time because we have to travel through every node
+# space because for worst case scenario, it recurses through every node before it returns in a single branche tree.
+def nodeDepths(root):
+    # Write your code here.
+    return recNodes(root,0,0)
+
+def recNodes(root,depth,sum):
+    sum += depth
+    leftSum,rightSum = 0,0
+    if root.left is not None:
+        leftSum = recNodes(root.left,depth+1,0)
+    if root.right is not None:
+        rightSum = recNodes(root.right,depth+1,0)
+    return sum+leftSum+rightSum
+
+# fail
 def nodeDepths(root):
     # recursion is the only way i know, we need multiple avenues
     # a variable to keep track of node depth to add
